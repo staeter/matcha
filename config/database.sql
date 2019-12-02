eaae12732e9aa47a80bccc2d741832eb03923631
STTR13 <simon.taeter@gmail.com>
2019-12-02 14:48:02 +0100

    rebuild db for it to fulfill the matcha requirements

--- config/database.sql
+++ config/database.sql
@@ -4,6 +4,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` VARCHAR(254) UNIQUE NOT NULL,
   `password` CHAR(128) NOT NULL,
 
+  `gender` INT UNSIGNED,
+  `orientation` INT UNSIGNED,
+  `biography` TEXT,
+  `birth` DATE,
+
+  `last_localisation` VARCHAR(64),
+  `last_log` DATETIME DEFAULT NOW(),
+  `is_loged` BOOLEAN DEFAULT false,
+
+  `popularity_score` INT,
+
   `pref_mail_notifications` BOOLEAN DEFAULT true,
 
   PRIMARY KEY (`id_user`),
@@ -14,11 +25,39 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 
 
+CREATE TABLE IF NOT EXISTS `intrests` (
+  `id_user` INT UNSIGNED,
+  `tag` VARCHAR(64),
+
+  UNIQUE KEY (`id_user`, `tag`),
+
+  FOREIGN KEY (`id_user`)
+  REFERENCES `user`(`id_user`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
+
+
+
+
 CREATE TABLE IF NOT EXISTS `account_verification` (
 	`account_verification_key` INT UNSIGNED NOT NULL,
 	`id_user` INT UNSIGNED UNIQUE NOT NULL,
 
 	UNIQUE KEY (`account_verification_key`, `id_user`),
+
+	FOREIGN KEY (`id_user`)
+    REFERENCES `user`(`id_user`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS `notifications` (
+	`id_user` INT UNSIGNED NOT NULL,
+	`notification` VARCHAR(254) NOT NULL DEFAULT "Something happened.",
 
 	FOREIGN KEY (`id_user`)
     REFERENCES `user`(`id_user`)
@@ -44,29 +83,11 @@ CREATE TABLE IF NOT EXISTS `account_retrieval_requests` (
 
 
 
-
--- CREATE TABLE IF NOT EXISTS `cookie` (
---   `id_cookie` INT UNSIGNED AUTO_INCREMENT NOT NULL,
---   `id_user` INT UNSIGNED,
---   `creation_date` DATETIME NOT NULL DEFAULT NOW(),
---
---   PRIMARY KEY (`id_cookie`),
---
---   FOREIGN KEY (`id_user`)
---   REFERENCES `user`(`id_user`)
---   ON DELETE CASCADE
---   ON UPDATE CASCADE
--- );
-
-
-
-
 CREATE TABLE IF NOT EXISTS `picture` (
   `id_picture` INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
   `id_user` INT UNSIGNED,
+  `is_profile-picture` BOOLEAN DEFAULT false,
   `path` VARCHAR(254),
-  `public` BOOLEAN DEFAULT false,
-  `date` DATETIME NOT NULL DEFAULT NOW(),
 
   PRIMARY KEY (`id_picture`),
 
@@ -80,19 +101,20 @@ CREATE TABLE IF NOT EXISTS `picture` (
 
 
 CREATE TABLE IF NOT EXISTS `like` (
-  `id_user` INT UNSIGNED,
-  `id_picture` INT UNSIGNED,
+  `id_user_liking` INT UNSIGNED,
+  `id_user_liked` INT UNSIGNED,
+  `liked` BOOLEAN DEFAULT true,
   `date` DATETIME NOT NULL DEFAULT NOW(),
 
-  UNIQUE KEY (`id_user`, `id_picture`),
+  UNIQUE KEY (`id_user_liking`, `id_user_liked`),
 
-  FOREIGN KEY (`id_user`)
+  FOREIGN KEY (`id_user_liking`)
   REFERENCES `user`(`id_user`)
   ON DELETE CASCADE
   ON UPDATE CASCADE,
 
-  FOREIGN KEY (`id_picture`)
-  REFERENCES `picture`(`id_picture`)
+  FOREIGN KEY (`id_user_liked`)
+  REFERENCES `user`(`id_user`)
   ON DELETE CASCADE
   ON UPDATE CASCADE
 );
@@ -100,22 +122,61 @@ CREATE TABLE IF NOT EXISTS `like` (
 
 
 
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id_comment` INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
-  `id_user` INT UNSIGNED NOT NULL,
-  `id_picture` INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `profile_viewed` (
+  `id_user_viewing` INT UNSIGNED,
+  `id_user_viewed` INT UNSIGNED,
   `date` DATETIME NOT NULL DEFAULT NOW(),
-  `content` TEXT,
 
-  PRIMARY KEY (`id_comment`),
-
-  FOREIGN KEY (`id_user`)
+  FOREIGN KEY (`id_user_viewing`)
   REFERENCES `user`(`id_user`)
   ON DELETE CASCADE
   ON UPDATE CASCADE,
 
-  FOREIGN KEY (`id_picture`)
-  REFERENCES `picture`(`id_picture`)
+  FOREIGN KEY (`id_user_viewed`)
+  REFERENCES `user`(`id_user`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS `report` (
+  `id_user_reporting` INT UNSIGNED,
+  `id_user_reported` INT UNSIGNED,
+  `description` TEXT,
+  `date` DATETIME NOT NULL DEFAULT NOW(),
+
+  FOREIGN KEY (`id_user_reporting`)
+  REFERENCES `user`(`id_user`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+
+  FOREIGN KEY (`id_user_reported`)
+  REFERENCES `user`(`id_user`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id_message` INT UNSIGNED AUTO_INCREMENT UNIQUE NOT NULL,
+  `id_user_sending` INT UNSIGNED,
+  `id_user_receiving` INT UNSIGNED,
+  `date` DATETIME NOT NULL DEFAULT NOW(),
+  `content` TEXT,
+
+  PRIMARY KEY (`id_message`),
+
+  FOREIGN KEY (`id_user_sending`)
+  REFERENCES `user`(`id_user`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+
+  FOREIGN KEY (`id_user_receiving`)
+  REFERENCES `user`(`id_user`)
   ON DELETE CASCADE
   ON UPDATE CASCADE
 );
