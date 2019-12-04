4ab2e75642146e1ddb382a8ae51f913aec4045ee
Reelbour <Redaelbouri54@gmail.com>
2019-12-04 18:16:42 +0100

    function is_login/is_logout set + fonction qui modifie dans la bdd : orientation, genre, bio

--- model/functions/modify_user_details.php
+++ model/functions/modify_user_details.php
@@ -6,11 +6,43 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/Matcha/model/classes/Database.class.p
 
 function  update_gender($id_user, $datatoinsert)
 {
-    $query = ('UPDATE user SET  WHERE id_user = :id VALUE ');
-    $db->query($query, array(':id' => $id_user));
+    $query = ('UPDATE user SET gender = :genre WHERE id_user = :id');
+    $db->query($query, array(':genre' => $datatoinsert, ':id' => $id_user));
 		$row = $db->fetch();
     return $row;
     // on retourne row qui contient toutes les infos contenu dans la table user
+}
+
+function update_sexuality_orientation($id_user, $datatoinsert)
+{
+  $query = ('UPDATE user SET orientation = :orientation WHERE id_user = :id VALUE ');
+  $db->query($query, array(':orientation' => $datatoinsert, ':id' => $id_user));
+  $row = $db->fetch();
+  return $row;
+  // on retourne row qui contient toutes les infos contenu dans la table user
+}
+
+function update_biography($id_user, $datatoinsert)
+{
+  $query = ('UPDATE user SET biography = :bio WHERE id_user = :id VALUE ');
+  $db->query($query, array(':bio' => $datatoinsert, ':id' => $id_user));
+  $row = $db->fetch();
+  return $row;
+  // on retourne row qui contient toutes les infos contenu dans la table user
+}
+
+function is_logged($id_user)
+{
+  $query = ('UPDATE user SET is_loged = :logged WHERE id_user = :id VALUE ');
+  $db->query($query, array(':logged' => TRUE, ':id' => $id_user));
+  $db->execute();
+}
+
+function is_logout($id_user)
+{
+  $query = ('UPDATE user SET is_loged = :logged WHERE id_user = :id VALUE ');
+  $db->query($query, array(':logged' => FALSE, ':id' => $id_user));
+  $db->execute();
 }
 
 
