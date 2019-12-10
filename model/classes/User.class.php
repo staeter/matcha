35fd20fa2c4a946c9b236e6c26c266428f3b0295
Reelbour <Redaelbouri54@gmail.com>
2019-12-10 18:15:49 +0100

    setting function : localisation, bio,

--- model/classes/User.class.php
+++ model/classes/User.class.php
@@ -354,18 +354,17 @@
 				$db->execute();
 		}
 
-
 		public function set_log($bool)
 		{
 		  //tester qu il capte bien la variable = a true or false
-		  $query = ('UPDATE user SET is_loged = :logged WHERE id_user = :id VALUE ');
+		  $query = ('UPDATE user SET is_loged = :logged WHERE id_user = :id');
 		  $db->query($query, array(':logged' => $bool, ':id' => $id_user));
 		  $db->execute();
 		}
 
 		public function set_sexuality_orientation($datatoinsert)
 		{
-		  $query = ('UPDATE user SET orientation = :orientation WHERE id_user = :id VALUE ');
+		  $query = ('UPDATE user SET orientation = :orientation WHERE id_user = :id');
 		  $db->query($query, array(':orientation' => $datatoinsert, ':id' => $this->_id));
 		  $db->execute();
 
@@ -373,10 +372,19 @@
 
 		public function set_biography($datatoinsert)
 		{
-		  $query = ('UPDATE user SET biography = :bio WHERE id_user = :id VALUE ');
+		  $query = ('UPDATE user SET biography = :bio WHERE id_user = :id');
 		  $db->query($query, array(':bio' => $datatoinsert, ':id' => $this->_id));
 		  $db->execute();
 		}
+
+		public function set_last_log()
+		{
+			$query = ('UPDATE user SET is_loged = :logged WHERE id_user = :id');
+		  $db->query($query, array(':logged' => $bool, ':id' => $id_user));
+		  $db->execute();
+		}
+
+
 
 		/*
 		** -------------------- Get --------------------
---
+++ model/functions/get_location.php
@@ -0,0 +1,40 @@
+<?php
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if (isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
+
+$PublicIP = get_client_ip();
+//$PublicIP = '8.8.8.8';
+$json     = file_get_contents("http://ipinfo.io/$PublicIP/geo");
+$json     = json_decode($json, true);
+
+
+function set_last_location($country, $town)
+{
+  $query = ('UPDATE user SET last_localisation = :last_loc WHERE id_user = :id');
+  $db->query($query, array(':bio' => $country .','. $town, ':id' => $this->_id));
+  $db->execute();
+}
+//$country  = $json['country'];
+//$region   = $json['region'];
+//$city     = $json['city'];
+
+?>
