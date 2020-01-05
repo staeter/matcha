e49e6c6ec011606b2d6bed988b72cc1bc0f9ab7e
Reelbour <Redaelbouri54@gmail.com>
2020-01-05 22:05:34 +0100

    starting setting update_password

--- control/update_password.php
+++ control/update_password.php
@@ -1,4 +1,30 @@
 <?php
+session_start();
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/model/classes/User.class.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/model/classes/Database.class.php';
+$db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
+
+if (empty($_POST['password']) || empty($_POST['confirm']))
+{
+    echo '{
+      "result" : "Failure",
+      "message" : "Vous devez remplir tout les champs."
+    }';
+}
+
+else if (strcmp($_POST['oldpw'], $_POST['newpw']) !== 0)
+{
+  echo '{
+    "result" : "Failure",
+    "message" : "Passwords doesnt match."
+  }';
+}
+else {
+
+  
+}
+
 //oldpw newpw
 /*
 {
