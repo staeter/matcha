385a3b49c9d5800d074fb10b119991add02aa3d4
Reelbour <Redaelbouri54@gmail.com>
2020-01-05 18:17:22 +0100

    signup almost done (still have to validate password but later)

--- control/signup.php
+++ control/signup.php
@@ -1,40 +1,25 @@
 <?php
-
-
 session_start();
 require '../model/classes/User.class.php';
 require '../model/functions/hash_password.php';
-$x =0;
 
+$x = 0;
 
-$pseudo = $_POST['pseudo'];
-$psw = $_POST['password'];
-$pswconfirm = $_POST['confirm'];
-$db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
+if (empty($_POST['password']) || empty($_POST['confirm']) || empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['email']) || empty($_POST['pseudo']))
+{
+  echo '{
+    "result" : "Failure",
+    "message" : "Vous devez remplir tout les champs."
+  }';
+}
+else {
 
-// if ($psw != $pswconfirm)
-// {
-//   echo '{
-//     "result" : "Failure",
-//     "message" : "The password and confirm must be the same !"
-//   }';
-// }
-//
-// else if (strlen($pseudo) < 3 || strlen($_POST['firstname']) < 3 || strlen($_POST['lastname']) < 3)
-//   {
-//     echo '{
-//       "result" : "Failure",
-//       "message" : "The pseudo / firstname / lastname must be more than 3 char!"
-//     }';
-//   }
-//
-// else {
-try {
-
-  $usr = new User($_POST['pseudo'], $_POST['firstname'], $_POST['lastname'], $_POST['email'], hash_password($_POST['password']), $db);
-  $x = 1;
-
-} catch (Exception $e) {
+  try {
+    $db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
+    $usr = new User($_POST['pseudo'], $_POST['firstname'], $_POST['lastname'], $_POST['email'], hash_password($_POST['password']), $db);
+    $x = 1;
+      }
+  catch (Exception $e) {
 
   if ($e->getCode() == 42)
     $x = 2;
@@ -43,25 +28,14 @@ try {
   else if ($e->getCode() == 43)
     $x = 4;
   else if ($e->getCode() == 44)
-    $x = 5;
-
-
-}
-
-
+    $x = 5;             }
+/*
 //
-// $pseudo = $_POST['pseudo'];
+//          Apres le try, selon le message d'erreur ou de réussite on renvoi le bon message JSON
 //
-// $c = strlen($pseudo);
-//
-// //if (isset($_POST['pseudo']))
-//   if ($c > 1)
-//   $x = 1;
-
-
-// if ((isset($_POST['pseudo'])) && (isset($_POST['firstname'])) && (isset($_POST['lastname']))
-//       && (isset($_POST['email'])) && (isset($_POST['password'])) && (isset($_POST['confirm'])))
-//     {
+//          Probleme de code, ne gere pas la validations du password, (diffuclté & si le meme que confirm)
+//           tout le monde peut avoir le meme pseudo
+*/
 
 if ($x == 1){
       echo '{
@@ -104,16 +78,10 @@ else if ($x == 5)
 else {
   echo '{
     "result" : "Failure",
-    "message" : "c mort de chez mort '.$e->getCode().'"
+    "message" : "probleme non determiné/gerer '.$e->getCode().'"
   }';
 }
-
-// pseudo lastname firstname email password confirm
-/*
-{
-  "result" : "Success" or "Failure",
-  "message" : "This is a message I want the user to see",
 }
-*/
+
 
 ?>
