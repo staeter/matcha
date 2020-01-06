968a9d05de37a16d33a51dfb59d4481fe091526a
Reelbour <Redaelbouri54@gmail.com>
2020-01-06 11:39:47 +0100

    need to recheck signout when button wil be up

--- control/signout.php
+++ control/signout.php
@@ -1,4 +1,44 @@
 <?php
+
+
+session_start();
+require '../model/classes/User.class.php';
+
+$x = 2;
+
+try {
+  $db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
+  $usr = new User($_SESSION['id'], $db);
+  $usr->is_logged(false);
+
+  $_SESSION['id'] = NULL;
+  $_SESSION['pseudo'] = NULL;
+  $_SESSION['mail'] = NULL;
+  $x = 1;
+  session_destroy();
+
+
+} catch (\Exception $e) {
+  $x = 0;
+
+}
+
+if ($x == 1){
+      echo '{
+        "result" : "Success",
+        "message" : "You been disconnect with success !"
+      }';
+    }
+else {
+  echo '{
+    "result" : "Failure",
+    "message" : "A problem occur when we trying to disconnect you !"
+  }';
+
+}
+
+
+
 /*
 {
   "result" : "Success" or "Failure",
