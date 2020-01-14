cf099e5e3486d0610cff5030d7036ab0dd9f86a8
Reelbour <Redaelbouri54@gmail.com>
2020-01-14 18:10:35 +0100

    update testing

--- model/classes/test.php
+++ model/classes/test.php
@@ -4,7 +4,6 @@ session_start();
 require $_SERVER["DOCUMENT_ROOT"] . '/model/classes/User.class.php';
 require $_SERVER["DOCUMENT_ROOT"] . '/model/functions/hash_password.php';
 
-$db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
 
 // if (empty($_POST['password']) || empty($_POST['confirm']))
 // {
@@ -24,16 +23,31 @@ $db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot'
 // else {
 
   try {
-    $id = 41;
-    $pw = 's';
-    $newpw = 'skssk12&skskksksoa';
-//User::send_account_retrieval
-  //  $usr = new User('iphone', hash_password($pw), $db);
-      if (User::is_valid_password($newpw) == true)
-      {
-        echo 'salut';
-      }
+    $db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
 
+  //  $id = 41;
+
+
+
+    $pw = 'sosa';
+    //$newpw = 'skssk12&skskksksoa';
+//User::send_account_retrieval
+
+
+
+
+   $usr = new User('sosa', hash_password($pw), $db);
+    $row1 = $usr->get_all_details();
+    $row2 = $usr->get_pref_mail_notifications();
+  print_r($row1);
+  echo '<br> <br>';
+  echo $row2;
+
+
+
+    //$usr = new User('sosa', hash_password('sosa'), $db);
+    //$row = $usr->get_all_details($id);
+    //  print_r($row);
     // echo '{
     //   "result" : "Success",
     //   "message" : "is valid hashed psw ! (object created)."
@@ -46,6 +60,80 @@ $db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot'
     }';
 
   }
+  // reucp de modif de user
+
+
+  session_start();
+  require $_SERVER["DOCUMENT_ROOT"] . '/model/classes/User.class.php';
+  require $_SERVER["DOCUMENT_ROOT"] . '/model/functions/hash_password.php';
+
+  $db = new Database('mysql:host=localhost:3306;dbname=matcha', 'root', 'rootroot');
+
+  try {
+    $usr = new User('sosa', hash_password($pw), $db);
+    $row = $usr->get_all_details();
+
+  } catch (\Exception $e) {
+    echo '{
+      "result" : "Failure",
+      "message" : "'.$e->getMessage().'"
+    }';
+  }
+
+
+
+
+  /*
+  {
+    "id" : 12,
+    "pseudo" : "myPseudo",
+    "first_name" : "John",
+    "last_name" : "Doe",
+    "gender" : "Man" or "Woman",
+    "orientation" : "Homosexual" or "Bisexual" or "Heterosexual",
+    "biography" : "blablabla",
+    "birth" : "some date",
+    "last_log" : "Now" or "some date",
+    "is_loged" : "True" or "False",
+    "pictures" : ["/data/name.png", ...],
+    "popularity_score" : 12,
+    "tags" : ["#tag", ...],
+    "liked" : "True" or "False",
+    -- mamybe "alert" : {
+    "result" : "Failure",
+    -- maybe "data" : {
+      "id" : "'.$row['id']}.'",
+      "pseudo" : '.$row['pseudo']}.',
+      "first_name" : '.$row['firstname']}.',
+      "last_name" : '.$row['lastname']}.',
+      "gender" : '.$row['gender']}.',
+      "orientation" : "Homosexual" or "Bisexual" or "Heterosexual",
+      "biography" : "blablabla",
+      "birth" : "some date",
+      "last_log" : "Now" or "some date",
+      "is_loged" : "True" or "False",
+      "pictures" : ["/data/name.png", ...],
+      "popularity_score" : 12,
+      "tags" : ["#tag", ...],
+      "liked" : "True" or "False"
+    }
+    -- maybe "alert" : {
+      "color" : "DarkRed",
+      "message" : "message for the user"
+    }
+
+  // public function get_all_details()
+  // {
+  //   $query = 'SELECT * FROM user WHERE id_user = :id';
+  //   $this->_db->query($query, array(':id' => $this->get_id()));
+  //   $row = $this->_db->fetch();
+  //   if ($row === false) {
+  //     throw new InvalidParamException("Failed running " . __METHOD__ . ". Id not found in database.");
+  //   }
+  //    return $row;
+  // }
+  //
+
 //
 // }
 
