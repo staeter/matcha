e15b78036384ed44fd14f1b0e80bcf186e9c0b57
Reelbour <Redaelbouri54@gmail.com>
2020-02-18 22:18:26 +0100

    setting current working well but tags still to be implemented(last things to do)

--- control/settings_current.php
+++ control/settings_current.php
@@ -11,54 +11,64 @@ $usr = unserialize($_SESSION['user']);
 
 $row = $usr->get_all_details();
 
+if ($row['gender'] == 1)
+  $gender = 'Man';
+else
+  $gender = 'Woman';
+
+if ($row['orientation'] == 0)
+  $orientation = 'Bisexual';
+else if ($row['orientation'] == 1)
+  $orientation = 'Heterosexual';
+else
+  $orientation = 'Homosexual';
+
+$rowpic = $usr->get_all_picture();
+
+//
 // echo '{
 //   "data" : {
 //     "pseudo" : "'.$row['pseudo'].'",
-//     "first_name" : "'.$row['firstname'].'",
-//     "last_name" : "'.$row['lastname'].'",
-//     "email"  : "'.$row['email'].'",
-//     "gender" : "'.$row['gender'].'",
-//     "orientation" : "'.$row['orientation'].'",
-//     "biography" : "'.$row['biography'].'",
-//     "birth" : "'.$row['birth'].'",
-//     "pictures" : ["/data/name.png", "/data/pic2.png"],
-//     "popularity_score" : '.$row['popularity_score'].',
-//     "tags" : ["joy", "stuff"],
+//     "first_name" : "'.$row['lastname'].'",
+//     "last_name" : "'.$row['firstname'].'",
+//     "email" : "'.$row['email'].'",
+//     "gender" : "'.$gender.'",
+//     "orientation" : "'.$orientation.'",
+//     "biography" : "Im good",
+//     "birth" : "01-01-1970",
+//     "pictures" :
+//       [ { "id" : 1,
+//           "path" : "https://images.unsplash.com/photo-1537886079430-486164575c54?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4c747db3353a34b312d05786f47930d3&auto=format&fit=crop&w=600&q=60"
+//         }
+//       , { "id" : 2,
+//           "path" : "https://images.unsplash.com/photo-1537886194634-e6b923f92ff1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9eb2726071e58c1b0a430a75b1047525&auto=format&fit=crop&w=600&q=60"
+//         }
+//       , { "id" : 6,
+//           "path" : "https://images.unsplash.com/photo-1537886243959-0b504cf58aa2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1171ce40e6e68e663c3399a67a915913&auto=format&fit=crop&w=600&q=60"
+//         }
+//       , { "id" : 12,
+//           "path" : "https://images.unsplash.com/photo-1537886492139-052c27acbfee?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=664282a4bd8b8a69cc860420214df3e7&auto=format&fit=crop&w=600&q=60"
+//         }
+//       , { "id" : 43,
+//           "path" : "https://images.unsplash.com/photo-1537886464786-8a0d500b0da6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=49984d393482456ea5484c3482cc52a9&auto=format&fit=crop&w=600&q=60"
+//         }
+//       ],
+//     "popularity_score" : 100,
+//     "tags" : ["tag", "lovetags"]
 //   },
 //   "alert" : {
 //     "color" : "DarkBlue",
-//     "message" : "Current setting alert"
+//     "message" : "current settings alert"
 //   }
 // }';
-
-
-////
-
-if ($row['gender'] == 1)
-  $gender = 'man';
-else
-  $gender = 'woman';
-
-if ($row['orientation'] == 0)
-  $orientation = 'bisexual';
-else if ($row['orientation'] == 1)
-  $orientation = 'heterosexual';
-else
-  $orientation = 'homosexual';
-
-////
-
 //
-// ******************** I NEED SOME FUNCTION HERE FOR GET the id picture and path
-//
-
-$rowpic = $usr->get_all_picture();
+// return;
 
 echo '{
 "data" : {
   "pseudo" : "'.$row['pseudo'].'",
-  "first_name" : "'.$row['firstname'].'",
   "last_name" : "'.$row['lastname'].'",
+  "first_name" : "'.$row['firstname'].'",
   "email" : "'.$row['email'].'",
   "gender" : "'.$gender.'",
   "orientation" : "'.$orientation.'",
@@ -89,27 +99,6 @@ echo '{
   "message" : "current settings alert"
 }
 }';
-
-
-
-//   "data" : {
-//     "pseudo" : "myPseudo",
-//     "first_name" : "John",
-//     "last_name" : "Doe",
-//     "email" : "Doe@gmail.com",
-//     "gender" : "Man",
-//     "orientation" : "Bisexual",
-//     "biography" : "Im good",
-//     "birth" : "01-01-1970",
-//     "pictures" : ["/data/pic1.png", "/data/pic2.png", "/data/pic3.png"],
-//     "popularity_score" : 100,
-//     "tags" : ["#tag", "lovetags"]
-//   },
-//   "alert" : {
-//     "color" : "DarkBlue",
-//     "message" : "current settings alert"
-//   }
-// }';
 
 
 
